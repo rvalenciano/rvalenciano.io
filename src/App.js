1c58629a0b3cfccefb771512b192cb67cfaae141
Randall Valenciano <ravf.226@gmail.com>
2020-11-06 07:18:02 -0600

    stage: adds stage change for demo purposes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <header>
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
+        <h3>Environment: STAGE and UAT</h3>
       </header>
       <AmplifySignOut />
     </div>
