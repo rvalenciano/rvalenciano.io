ccad7d6433b68327c68b15aabd4f326cc5e78add
Randall Valenciano <ravf.226@gmail.com>
2020-11-06 05:31:17 -0600

    dev: adds environment flag

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
+  useEffect(() => {
+    document.title = "DEV Env"
+ }, []);
+
   return (
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
+        <h3>Environment: DEV</h3>
       </header>
       <AmplifySignOut />
     </div>
